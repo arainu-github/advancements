608b1a9354446054ea0890720bb19168ae5c5e0e
Riku Ueda <riku.ueda@outlook.jp>
2021-12-29 09:16:14 +0900

    PEP準拠

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def append(avt, advancements, ended, r, overwrite=False):
 
 
 @app.route('/')
-def get_advancements(request):
+def get_advancements(_):
     cur = conn.cursor()
     cur.execute("SELECT * FROM `advancement`")
     advancements = cur.fetchall()
